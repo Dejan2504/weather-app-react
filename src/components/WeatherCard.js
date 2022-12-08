79d5cfc27ff97b48324016e6c8e213ec3b4e667a
Dejan2504 <krajisnikdejan@gmail.com>
2022-12-08 18:20:10 +0100

    Data converter

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -2,17 +2,16 @@ import React from 'react';
 
 const WeatherCard = function({weatherData}) {
   const icon = weatherData.weather[0].icon;
-  console.log(icon);
 
   return(
     <div>  
     <h1>{weatherData.name}</h1>
-    <span>{weatherData.main.temp}</span><span><img src={`http://openweathermap.org/img/wn/${icon}@2x.png`}></img></span>
-        <div>Feels like: {weatherData.main.feels_like}</div>
-        <div>Max temperature: {weatherData.main.temp_max}</div>
-        <div>Min temperature: {weatherData.main.temp_min}</div>
-        <div>Pressure: {weatherData.main.pressure}</div>
-        <div>Humidity: {weatherData.main.humidity}</div>
+    <span>{Math.round(weatherData.main.temp - 272.15) + '℃'}</span><span><img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt='icon'></img></span>
+        <div>Feels like: {Math.round(weatherData.main.feels_like - 272.15) + '℃'}</div>
+        <div>Max temperature: {Math.round(weatherData.main.temp_max - 272.15) + '℃'}</div>
+        <div>Min temperature: {Math.round(weatherData.main.temp_min - 272.15) + '℃'}</div>
+        <div>Pressure: {weatherData.main.pressure + 'hPa'}</div>
+        <div>Humidity: {weatherData.main.humidity + '%'}</div>
     </div>
     );
 };
